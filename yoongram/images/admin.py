74625872ef9185561c8a236171f257e9f94eb633
happyjy <okwoyjy@gmail.com>
2019-02-05 17:26:06 +0900

    customizing the Django Admin panel - __str__/ change admin model with model meta option

--- yoongram/images/admin.py
+++ yoongram/images/admin.py
@@ -1,3 +1,58 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+@admin.register(models.Image)
+class ImageAdmin(admin.ModelAdmin):
+  # admin > Imageadmin 패널에 템플릿을 설정해주기 위해서 
+  # pass를 주석 처리하고 아래와 같은 설정을 함. 
+  # pass 
+
+  # Move to Link to modifiy comtents
+  list_display_links = (
+    'location',
+    'caption',
+  )
+
+  search_fields = (
+    'location',
+    'created_at',
+    'updated_at',
+  )
+
+  list_filter = (
+    'location',
+    'creator',
+
+  )
+
+  # Create table list Image admin page 
+  list_display = (
+    'file',
+    'location',
+    'caption',
+    'creator',
+    'created_at',
+    'updated_at',
+  )
+@admin.register(models.Like)
+class LikeAdmin(admin.ModelAdmin):
+  # pass
+  list_display = (
+    'creator',
+    'image',
+    'created_at',
+    'updated_at',
+  )
+
+@admin.register(models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+  # pass
+  list_display = (
+    'message',
+    'creator',
+    'image',
+    'created_at',
+    'updated_at',
+  )
+
--- yoongram/images/models.py
+++ yoongram/images/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from yoongram.users import models as user_model
+from django.utils.encoding import python_2_unicode_compatible
+
 # Create your models here.
+@python_2_unicode_compatible
 class TimeStampedModel(models.Model):
 
   created_at = models.DateTimeField(auto_now_add=True)
@@ -8,7 +11,7 @@ class TimeStampedModel(models.Model):
 
   class Meta: 
     abstract = True
-
+@python_2_unicode_compatible
 class Image(TimeStampedModel):
   
   """ Image Model """
@@ -18,6 +21,9 @@ class Image(TimeStampedModel):
   caption = models.TextField()
   creator = models.ForeignKey(user_model.User, on_delete=models.PROTECT, null=True)
 
+  def __str__(self):
+    return '{} - {}'.format(self.location, self.caption)
+@python_2_unicode_compatible
 class Comment(TimeStampedModel):
   
   """ Comment Model """
@@ -26,10 +32,16 @@ class Comment(TimeStampedModel):
   creator = models.ForeignKey(user_model.User, on_delete=models.PROTECT, null=True)
   image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
 
+  def __str__(self):
+    return '메세지 압니다. : {}'.format(self.message)
 
+@python_2_unicode_compatible
 class Like(TimeStampedModel):
 
   """ Like Model """
 
   creator = models.ForeignKey(user_model.User, on_delete=models.PROTECT, null=True)
   image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
+
+  def __str__(self):
+    return 'User: {} - Image caption: {}'.format(self.creator.username, self.image.caption)
