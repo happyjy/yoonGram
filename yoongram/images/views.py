f052e931f3073dd15c4da29577f4bb545543c325
happyjy <okwoyjy@gmail.com>
2019-02-09 19:35:28 +0900

    #1-38 making query

--- yoongram/images/views.py
+++ yoongram/images/views.py
@@ -22,12 +22,24 @@ class ListAllImages(APIView):
 
 class ListAllComments(APIView):
   def get(self, request,format=None):
+    # 로그인한 유저가 생선한 댓글만 보기작업
+    # user_id = request.user.id
+    # all_comments = models.Comment.objects.filter(creator=user_id)
+
+    # #1-38 query
+    # all_comments = models.Comment.objects.filter(creator=1)
+    # all_comments = models.Comment.objects.filter(id=2)
+    
     all_comments = models.Comment.objects.all()
     serializer = serializers.CommentSerializer(all_comments, many=True)
     return Response(data=serializer.data)
 
 class ListAllLikes(APIView):
   def get(self, request, format=None):
+
+    # check request value 
+    print(request.scheme)
+
     all_Like = models.Like.objects.all()
     serializer = serializers.LikeSerializer(all_Like, many=True)
     return Response(data=serializer.data)
