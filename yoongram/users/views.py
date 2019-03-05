d3920d6580c0e7375080c1f72357a86dc7edb825
happyjy <okwoyjy@gmail.com>
2019-03-05 22:17:38 +0900

    #1-67 Update Profile

--- yoongram/users/views.py
+++ yoongram/users/views.py
@@ -47,19 +47,71 @@ class UnFollowUser(APIView):
 
 
 class UserProfile(APIView):
-  def get(self, request, username, format=None):
+  def get_user(self, username):
+    
     try:
       found_user = models.User.objects.get(username=username)
-
-      # create notifications for like 
-
+      return found_user
     except models.User.DoesNotExist:
-      return Response(status=status.HTTP_404_NOT_FOUND)
+      return None
+  
+  def get(self, request, username, format=None):
+    print(username)
+    found_user = self.get_user(username)
 
+    print("### UserProfile get ###")
+    print(found_user)
+    if found_user is None:
+      return Response(status=status.HTTP_404_NOT_FOUND)
+    
     serializer = serializers.UserProfileSerializer(found_user)
 
     print(serializer.data)
     return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+  def put(self, request, username, format=None):
+    user = request.user
+
+    print("### user DATA")
+    print(request)
+    print(user) #yjy
+    print(user.username) #yjy
+
+    found_user = self.get_user(username)
+
+    if found_user is None:
+      return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    elif found_user.username != user.username:
+      return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+    else:
+      serializer = serializers.UserProfileSerializer(
+        found_user, data=request.data, partial=True)
+
+      print("### serializer : ", serializer)
+    #     UserProfileSerializer(<User: yjy>, data={'bio': 'abd'}, partial=True):
+    #     profile_image = ImageField(allow_null=True, max_length=100, required=False)
+    #     username = CharField(help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.', max_length=150, validators=[<django.contrib.auth.validators.UnicodeUsernameValidator object>, <UniqueValidator(queryset=User.objects.all())>])
+    #     name = CharField(allow_blank=True, label='Name of User', max_length=255, required=False)
+    #     bio = CharField(allow_null=True, required=False, style={'base_template': 'textarea.html'})
+    #     website = URLField(allow_null=True, max_length=200, required=False)
+    #     post_count = ReadOnlyField()
+    #     followers_count = ReadOnlyField()
+    #     following_count = ReadOnlyField()
+    #     images = CountImageSerializer(many=True):
+    #     id = IntegerField(label='ID', read_only=True)
+    #     file = ImageField(max_length=100)
+    #     comment_count = ReadOnlyField()
+    #     like_count = ReadOnlyField()
+      print("### found_user : ", found_user) #yjy
+      print("### request.data : ", request.data) #변경data eg {"bio": "abc"}
+
+      if serializer.is_valid():
+        serializer.save()
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+      else:
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserFollowerUser(APIView):
@@ -106,10 +158,12 @@ class UserFollowingUser(APIView):
 
 #     return Response(data=serializer.data, status=status.HTTP_200_OK)
 
-
 class Search(APIView):
   def get(self, request, format=None):
+    # http://127.0.0.1:8000/users/search/?username=y
     username = request.query_params.get('username', None)
+    
+    print("### SEARCH ###")
     print(username)
     if username is not None:
       users = models.User.objects.filter(username__istartswith=username)
