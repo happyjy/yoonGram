bc7f8df61a2e42d29c62c549ba7b780a9cf2b6f4
happyjy <okwoyjy@gmail.com>
2019-03-10 11:58:54 +0900

    #1-70 Setting up JWT

--- yoongram/users/serializers.py
+++ yoongram/users/serializers.py
@@ -14,7 +14,6 @@ class UserProfileSerializer(serializers.ModelSerializer):
   # : 이 serialize를 만들때 자동으로 만드는 필드 
   # : 예로 readonly해준 필드는 수정 불가능 한 필드로 수정 불가능한 필드로 인식하게 한다.
   # https://www.django-rest-framework.org/api-guide/fields/#readonlyfield
-  #
   post_count = serializers.ReadOnlyField()
   followers_count = serializers.ReadOnlyField()
   following_count = serializers.ReadOnlyField()
