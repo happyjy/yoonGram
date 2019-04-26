d991408ae32465aea1e0a91959e4ae7ceac834f0
happyjy <okwoyjy@gmail.com>
2019-04-27 01:40:06 +0900

    #3-0 Creating the User Reducer

---
+++ frontend/src/redux/modules/users.js
@@ -0,0 +1,43 @@
+/**
+ *   # redux 
+ * 
+ *   imports
+ *   actions
+ *   actions creators
+ *   initial state
+ *   reducer
+ *   reducer function
+ *   exports
+ *   reducer export
+ * 
+ * */
+
+
+
+
+
+// imports
+
+// actions
+
+// actions creators
+
+// initial state
+const initialState = {
+  isLoggedIn: localStorage.getItem("jwt") || false
+};
+
+// reducer
+function reducer( state = initialState, action){
+  switch (actions.type){
+    default: 
+      return state;
+  }
+}
+
+// reducer function
+
+// exports
+
+// reducer export
+export default reducer;
