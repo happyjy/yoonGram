ad522a59e9c6a4c50b611304d88fc138c61c5f93
happyjy <okwoyjy@gmail.com>
2019-06-28 07:31:50 +0900

    #3-51 FeedPhoto Component pt.Two

--- frontend/src/components/FeedPhoto/presenter.js
+++ frontend/src/components/FeedPhoto/presenter.js
@@ -1,11 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from './styles.module.scss';
+import PhotoActions from "components/PhotoActions";
 
 
 const FeedPhoto = props => {
-  console.log("### FeedPhoto props: " , props);
-  return <div className={styles.feedPhoto}>hello!</div>;
+  // console.log("### FeedPhoto > presenter props: " , props);
+  return (
+    <div className={styles.feedPhoto}>
+      <header className={styles.header}>
+        <img 
+          src={props.creator.profile_image || require("images/noPhoto.png")}
+          alt={props.creator.username}
+          className={styles.image}/>          
+        <div className={styles.headerColumn}>
+          <span className={styles.creator}>{props.creator.username}</span>
+          <span className={styles.location}>{props.location}</span>
+        </div>
+      </header>
+      <img src={props.file} alt={props.caption}/>
+      <div>
+          <PhotoActions number={props.like_count}/>
+      </div>
+    </div>
+  );
 };
 
 //위 props를 설정하는것입니다.
