5276adb49bf5a6894631db25e08854b9f08dd8a9
happyjy <okwoyjy@gmail.com>
2019-06-30 11:27:56 +0900

    #3-53 Making the comments resizeable

--- frontend/src/components/CommentBox/presenter.js
+++ frontend/src/components/CommentBox/presenter.js
@@ -1,11 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Textarea from "react-textarea-autosize";
 import styles from "./styles.module.scss";
 
 const CommentBox = (props, context) => (
-  <from>
-    <textarea placeholder={context.t("Add a comment...")} />
-  </from>
+  <form>
+    <Textarea 
+      className={styles.input}
+      placeholder={context.t("Add a comment...")} 
+    />
+  </form>
 );
 
 CommentBox.contextTypes = {
